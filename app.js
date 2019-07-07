8c0c3a6ad273b0d0d0af7d587c695c458383cad4
Nathan <airide101@gmail.com>
2019-07-07 15:25:24 -0400

    Added node backend.

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname +'/public'));
+
+app.get("/", function(req, res){
+	res.render("index.ejs");
+});
+
+app.listen(3000, function(){
+    console.log("server started.......");
+});
