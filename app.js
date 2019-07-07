e3fd27a5037e6b580d7711b14ea73881bbf3d843
Nathan <airide101@gmail.com>
2019-07-07 15:38:56 -0400

    Added other linked projects to site.

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ app.get("/", function(req, res){
 	res.render("index.ejs");
 });
 
+app.get("/colorgame", function(req, res){
+	res.render("colorgame.ejs");
+});
+
+app.get("/countries", function(req, res){
+	res.render("countries.ejs");
+});
+
+app.get("/projectboost", function(req, res){
+	res.render("ProjectBoost/index.ejs");
+});
+
 app.listen(3000, function(){
     console.log("server started.......");
 });
